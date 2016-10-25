7688333aacc02672bddfbc2b22133f9e4bb59533
mohayonao <mohayonao@gmail.com>
2016-10-25 16:57:54 +0900

    use decorator > Panel

--- src/client/components/FrontPanel.js
+++ src/client/components/FrontPanel.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Panel from "./ui/Panel";
 import { NUMBER_OF_COLS } from "../../common/constants";
 import { toCX, toCY } from "../designer";
 import { PadLayout, PadCaptions } from "../designer";
 
-export default class FacePanel extends Panel {
-  static defaultProps = { render };
-}
+@Panel()
+export default class FacePanel extends Component {
+  render() {
+    const elems = [];
 
-function render() {
-  const elems = [];
+    PadLayout.forEach((rowData, row) => {
+      rowData.forEach((pad, col) => {
+        if (!PadCaptions[pad]) {
+          return;
+        }
+        const caption = PadCaptions[pad];
+        const key = row * NUMBER_OF_COLS + col;
+        const cx = toCX(col);
+        const cy = toCY(row);
 
-  PadLayout.forEach((rowData, row) => {
-    rowData.forEach((pad, col) => {
-      if (!PadCaptions[pad]) {
-        return;
-      }
-      const caption = PadCaptions[pad];
-      const key = row * NUMBER_OF_COLS + col;
-      const cx = toCX(col);
-      const cy = toCY(row);
-
-      elems.push(
-        <g key={ key }>
-          <circle cx={ cx } cy={ cy } r={ 30 } fill="#050504"/>
-          <text x={ cx } y={ cy } fill="#f0f0f0" textAnchor="middle" dominantBaseline="middle">
-            { caption }
-          </text>
-        </g>
-      );
+        elems.push(
+          <g key={ key }>
+            <circle cx={ cx } cy={ cy } r={ 30 } fill="#050504"/>
+            <text x={ cx } y={ cy } fill="#f0f0f0" textAnchor="middle" dominantBaseline="middle">
+              { caption }
+            </text>
+          </g>
+        );
+      });
     });
-  });
 
-  return elems;
+    return (<g>{ elems }</g>);
+  }
 }
