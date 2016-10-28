1aab27b06c410d245b8282092d467176ad7c9eb0
mohayonao <mohayonao@gmail.com>
2016-10-29 07:23:22 +0900

    clean up code

--- src/client/components/LPadMatrix.js
+++ src/client/components/LPadMatrix.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from "react";
-import LPadRow from "./LPadRow";
+import LPadPadRect from "./LPadPadRect";
+import LPadPadOval from "./LPadPadOval";
+import { toCX, toCY, PadLayout } from "../designer";
 
 export default class LPadMatrix extends Component {
   static propTypes = {
@@ -22,5 +24,46 @@ export default class LPadMatrix extends Component {
     });
 
     return (<g className="lpad-matrix">{ elems }</g>);
+  }
+}
+
+export class LPadRow extends Component {
+  static propTypes = {
+    row          : PropTypes.number.isRequired,
+    data         : PropTypes.arrayOf(PropTypes.number).isRequired,
+    onValueChange: PropTypes.func.isRequired,
+  };
+
+  constructor(...args) {
+    super(...args);
+
+    this._layout = PadLayout[this.props.row].map(pad => pad.charAt(0)).map((ch) => {
+      return { "[": LPadPadRect, "(": LPadPadOval }[ch] || null;
+    });
+  }
+
+  shouldComponentUpdate(nextProp) {
+    return nextProp.data !== this.props.data;
+  }
+
+  render() {
+    const { row, data } = this.props;
+    const elems = [];
+
+    data.forEach((colData, col) => {
+      const LPadPad = this._layout[col] || null;
+
+      if (LPadPad !== null) {
+        const cx = toCX(col);
+        const cy = toCY(row);
+        const onValueChange = this.props.onValueChange.bind(null, col);
+
+        elems.push(
+          <LPadPad key={ col } cx={ cx } cy={ cy } data={ colData } onValueChange={ onValueChange }/>
+        );
+      }
+    });
+
+    return (<g className="lpad-row">{ elems }</g>);
   }
 }
